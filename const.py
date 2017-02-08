129b48f77325b92c18a2cc541f1612941d90b0bc
edenYerushalmy <edenyerushalmy@gmail.com>
2017-02-08 21:02:49 +0200

    no message

--- const.py
+++ const.py
@@ -9,11 +9,12 @@ dest_country_key = "DestCountry"
 orig_country_key = "OrigCountry"
 
 start_year = 2007
-number_of_seasons = 9
+number_of_seasons = 10
 
 # array of tuples each tuple is : (int: year , string: json file path contains all transferd for this year)
 all_years_files_path = []
 [all_years_files_path.append((start_year+i, "Transfers_"+str(start_year+i)+"_"+str(start_year+i+1)+".json")) for i in range(number_of_seasons)]
-
+for y in all_years_files_path:
+    print y
 
 data_dir_path = "crawlerData"
